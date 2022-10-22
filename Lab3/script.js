7efb452735fc87d8d69321e76d9453dbe3851221
kpatel115 <kp9338448@gmail.com>
2022-10-22 14:30:35 -0500

    added event listener to range input

--- Lab3/script.js
+++ Lab3/script.js
@@ -1,11 +1,9 @@
-let btn = document.getElementById("btn");
-let bill = document.getElementById("bill");
+let slider = document.getElementById("tip");
 
-function tipCalc(event){
+slider.addEventListener("change", function(event){
   let bill = document.getElementById("bill").value;
   if(bill === "" || bill < 1 || isNaN(bill)){
     alert("please enter valid number");
-
   } else {
   let tip = document.getElementById("tip").value;
   console.log(bill);
@@ -15,27 +13,25 @@ function tipCalc(event){
   let billTotal = document.getElementById("billWithTip").value = parseFloat(bill) + parseFloat(tipAmount);
   event.preventDefault();
   }
-}
-// --------------------------------------------------
-// bill.addEventListener("click", function(event){
-  
-//   let bill = document.getElementById("bill").value;
-//   if(bill === "" || bill < 1 || isNaN(bill)){
-//     alert("please enter valid number");
+});
 
-//   } else {
-//   let tip = document.getElementById("tip").value;
-//   console.log(bill);
-//   console.log(tip);
-//   let tipPercent = document.getElementById("tipPercentage").value = tip;
-//   let tipAmount = document.getElementById("tipAmount").value = bill * (tip/100);
-//   let billTotal = document.getElementById("billWithTip").value = parseFloat(bill) + parseFloat(tipAmount);
-//   event.preventDefault();
-//   }
-// })
+function tipCalc(event){
+  let bill = document.getElementById("bill").value;
+  if(bill === "" || bill < 1 || isNaN(bill)){
+    alert("please enter valid number");
+  } else {
+  let tip = document.getElementById("tip").value;
+  console.log(bill);
+  console.log(tip);
+  let tipPercent = document.getElementById("tipPercentage").value = tip;
+  let tipAmount = document.getElementById("tipAmount").value = bill * (tip/100);
+  let billTotal = document.getElementById("billWithTip").value = parseFloat(bill) + parseFloat(tipAmount);
+  event.preventDefault();
+  }
+};
 
-//---------------------------------------------------
-
+//-------------SUBMIT BUTTON--------------------------------
+// let btn = document.getElementById("btn");
 // btn.addEventListener("click", function(event){
   
 //   let bill = document.getElementById("bill").value;
