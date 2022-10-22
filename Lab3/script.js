b139f1e7b52bf9c78ff9dde899fdcecb50d155f0
kpatel115 <kp9338448@gmail.com>
2022-10-22 14:37:42 -0500

    shortened code, changed variable names

--- Lab3/script.js
+++ Lab3/script.js
@@ -6,11 +6,11 @@ slider.addEventListener("change", function(event){
     alert("please enter valid number");
   } else {
   let tip = document.getElementById("tip").value;
-  console.log(bill);
-  console.log(tip);
   let tipPercent = document.getElementById("tipPercentage").value = tip;
-  let tipAmount = document.getElementById("tipAmount").value = bill * (tip/100);
-  let billTotal = document.getElementById("billWithTip").value = parseFloat(bill) + parseFloat(tipAmount);
+  let tipTotal = bill * (tip/100);
+  let tipAmount = document.getElementById("tipAmount").value = tipTotal.toFixed(2);
+  let totalWithTip = parseFloat(bill) + parseFloat(tipAmount);
+  let billTotal = document.getElementById("billWithTip").value = totalWithTip.toFixed(2);
   event.preventDefault();
   }
 });
@@ -24,27 +24,10 @@ function tipCalc(event){
   console.log(bill);
   console.log(tip);
   let tipPercent = document.getElementById("tipPercentage").value = tip;
-  let tipAmount = document.getElementById("tipAmount").value = bill * (tip/100);
-  let billTotal = document.getElementById("billWithTip").value = parseFloat(bill) + parseFloat(tipAmount);
+  let tipTotal = bill * (tip/100);
+  let tipAmount = document.getElementById("tipAmount").value = tipTotal.toFixed(2);
+  let total = parseFloat(bill) + parseFloat(tipAmount);
+  let billTotal = document.getElementById("billWithTip").value = total.toFixed(2);
   event.preventDefault();
   }
 };
-
-//-------------SUBMIT BUTTON--------------------------------
-// let btn = document.getElementById("btn");
-// btn.addEventListener("click", function(event){
-  
-//   let bill = document.getElementById("bill").value;
-//   if(bill === "" || bill < 1 || isNaN(bill)){
-//     alert("please enter valid number");
-
-//   } else {
-//   let tip = document.getElementById("tip").value;
-//   console.log(bill);
-//   console.log(tip);
-//   let tipPercent = document.getElementById("tipPercentage").value = tip;
-//   let tipAmount = document.getElementById("tipAmount").value = bill * (tip/100);
-//   let billTotal = document.getElementById("billWithTip").value = parseFloat(bill) + parseFloat(tipAmount);
-//   event.preventDefault();
-//   }
-// })
