c46404d6c3f19a76641882ca4d96fd333510f9dc
kpatel115 <kp9338448@gmail.com>
2022-10-22 14:06:16 -0500

    Added form validation

--- Lab3/script.js
+++ Lab3/script.js
@@ -1,7 +1,12 @@
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function(event){
+  
   let bill = document.getElementById("bill").value;
+  if(bill === "" || bill < 1 || isNaN(bill)){
+    alert("please enter valid number");
+
+  } else {
   let tip = document.getElementById("tip").value;
   console.log(bill);
   console.log(tip);
@@ -9,4 +14,5 @@ btn.addEventListener("click", function(event){
   let tipAmount = document.getElementById("tipAmount").value = bill * (tip/100);
   let billTotal = document.getElementById("billWithTip").value = parseFloat(bill) + parseFloat(tipAmount);
   event.preventDefault();
+  }
 })
