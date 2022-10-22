f47790eb10415eca7727d92cff8dbdaeec125431
kpatel115 <kp9338448@gmail.com>
2022-10-22 14:18:50 -0500

    added onkeyup event to bill input

--- Lab3/script.js
+++ Lab3/script.js
@@ -1,7 +1,7 @@
 let btn = document.getElementById("btn");
+let bill = document.getElementById("bill");
 
-btn.addEventListener("click", function(event){
-  
+function tipCalc(event){
   let bill = document.getElementById("bill").value;
   if(bill === "" || bill < 1 || isNaN(bill)){
     alert("please enter valid number");
@@ -15,4 +15,40 @@ btn.addEventListener("click", function(event){
   let billTotal = document.getElementById("billWithTip").value = parseFloat(bill) + parseFloat(tipAmount);
   event.preventDefault();
   }
-})
+}
+// --------------------------------------------------
+// bill.addEventListener("click", function(event){
+  
+//   let bill = document.getElementById("bill").value;
+//   if(bill === "" || bill < 1 || isNaN(bill)){
+//     alert("please enter valid number");
+
+//   } else {
+//   let tip = document.getElementById("tip").value;
+//   console.log(bill);
+//   console.log(tip);
+//   let tipPercent = document.getElementById("tipPercentage").value = tip;
+//   let tipAmount = document.getElementById("tipAmount").value = bill * (tip/100);
+//   let billTotal = document.getElementById("billWithTip").value = parseFloat(bill) + parseFloat(tipAmount);
+//   event.preventDefault();
+//   }
+// })
+
+//---------------------------------------------------
+
+// btn.addEventListener("click", function(event){
+  
+//   let bill = document.getElementById("bill").value;
+//   if(bill === "" || bill < 1 || isNaN(bill)){
+//     alert("please enter valid number");
+
+//   } else {
+//   let tip = document.getElementById("tip").value;
+//   console.log(bill);
+//   console.log(tip);
+//   let tipPercent = document.getElementById("tipPercentage").value = tip;
+//   let tipAmount = document.getElementById("tipAmount").value = bill * (tip/100);
+//   let billTotal = document.getElementById("billWithTip").value = parseFloat(bill) + parseFloat(tipAmount);
+//   event.preventDefault();
+//   }
+// })
