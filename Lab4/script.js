72a18272d7806d6824732c55594ebcd5fef8ede9
kpatel115 <kp9338448@gmail.com>
2022-11-04 13:38:37 -0500

    setup template for rendering object from api

--- Lab4/script.js
+++ Lab4/script.js
@@ -1,8 +1,11 @@
 // Geolocation API
 navigator.geolocation.getCurrentPosition((position) => {
   function doSomething(a, b){
-    console.log(a);
-    console.log(b);
+    let currentLatitude = a;
+    let currentLongitude = b;
+    fetch("https://weatherdbi.herokuapp.com/data/weather/"+ currentLatitude + "," + currentLongitude)
+    .then(res=>res.json())
+    .then(json=>console.log(JSON.stringify(json)))
   };
   doSomething(position.coords.latitude, position.coords.longitude);
 });
@@ -10,7 +13,7 @@ navigator.geolocation.getCurrentPosition((position) => {
 
 
 
-// Weather Web API
-fetch("https://weatherdbi.herokuapp.com/data/weather/41.8446119,-87.6285002")
-    .then(res=>res.json())
-    .then(json=>console.log(json))
+// // Weather Web API
+// fetch("https://weatherdbi.herokuapp.com/data/weather/41.8446119,-87.6285002")
+//     .then(res=>res.json())
+//     .then(json=>console.log(json))
