6e8f8c3e9a1a054c6ca2351452f2882fc766fec5
kpatel115 <kp9338448@gmail.com>
2022-11-07 15:39:24 -0600

    displayed current Weather data onto HTML page

--- Lab4/script.js
+++ Lab4/script.js
@@ -11,24 +11,78 @@ navigator.geolocation.getCurrentPosition((position) => {
   doSomething(position.coords.latitude, position.coords.longitude);
 });
 
+function displayWeather(arg){
+  // Current Weather Forecast
+  let dash_region = document.getElementById("region");
+  let dash_day = document.getElementById("day");
+  let dash_temp = document.getElementById("temp");
+  let dash_precip = document.getElementById("precip");
+  let dash_humidity = document.getElementById("humidity");
+  let dash_wind = document.getElementById("wind");
+  let dash_icon = document.getElementById("icon");
+  let dash_comment = document.getElementById("comment");
+
+  dash_region.innerHTML = arg.region;
+  dash_day.innerHTML = arg.currentConditions.dayhour;
+  dash_temp.innerHTML = arg.currentConditions.temp.f + " °F";
+  dash_precip.innerHTML = "Precipitation: " + arg.currentConditions.precip;
+  dash_humidity.innerHTML = "Humidity: " + arg.currentConditions.humidity;
+  dash_wind.innerHTML = "Wind: " + arg.currentConditions.wind.mile + " mph";
+  dash_icon.innerHTML = arg.currentConditions.iconURL;
+  dash_comment.innerHTML = arg.currentConditions.comment;
+
+  // 7 Day Forecast
+  // let dash_region = document.getElementById("region");
+  // let dash_day = document.getElementById("day");
+  // let dash_temp = document.getElementById("temp");
+  // let dash_precip = document.getElementById("precip");
+  // let dash_humidity = document.getElementById("humidity");
+  // let dash_wind = document.getElementById("wind");
+  // let dash_icon = document.getElementById("icon");
+  // let dash_comment = document.getElementById("comment");
+
+  // dash_region.innerHTML = arg.region;
+  // dash_day.innerHTML = arg.currentConditions.dayhour;
+  // dash_temp.innerHTML = arg.currentConditions.temp.f;
+  // dash_precip.innerHTML = arg.currentConditions.precip;
+  // dash_humidity.innerHTML = arg.currentConditions.humidity;
+  // dash_wind.innerHTML = arg.currentConditions.wind.mile;
+  // dash_icon.innerHTML = arg.currentConditions.iconURL;
+  // dash_comment.innerHTML = arg.currentConditions.comment;
+
+  }
 
 
 
 // Weather Web API
 function SearchWeatherForm() {
-
+  let weatherObject;
   let inputCity = document.getElementById("search-input").value;
 
   if (inputCity === ""){
     alert("Please enter valid city name");
-  } else {
+  } 
+  else {
+
     let refinedInputCity = inputCity.replace(/\s/g, "");
+
     // or 
     //let refinedInputCity1 = inputCity.split(" ").join("");
 
     fetch("https://weatherdbi.herokuapp.com/data/weather/" + refinedInputCity )
-    .then(res=>res.json())
-    .then(json=>console.log(json))
+    .then((res) => {
+     if (res.ok){
+      return res.json(); 
+     } else {
+      throw new Error("Network Response Error");
+    }})
 
+    .then( json => {
+      console.log(json);
+      displayWeather(json)
+    })
+
+    .catch ((error) => console.error("Fetch Error:", error));
   };
+  
 }
